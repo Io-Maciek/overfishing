ac42ea766b3650c15a30ec404d65d5de5402035f
Maciej <mkuchcik@gmail.com>
2024-04-06 14:31:46 +0200

    player canvas chooser animation added

--- Scripts/Menu/PlayerPanel.cs
+++ Scripts/Menu/PlayerPanel.cs
@@ -8,6 +8,7 @@ public class PlayerPanel : Control
 	Label Description;
 	public Control Arrows;
 	TextureRect fishTexture;
+	AnimationPlayer animation;
 
 	public override void _Ready()
 	{
@@ -21,6 +22,11 @@ public class PlayerPanel : Control
 
 		fishTexture = GetNode("FishTexture") as TextureRect;
 		fishTexture.Texture = null;
+
+		animation = GetNode("AnimationPlayer") as AnimationPlayer;
+
+
+		fishTexture.RectPivotOffset = this.RectSize / 2.0f;
 	}
 
 	public void SetPlayer(PlayerUIModel player = null)
@@ -30,12 +36,16 @@ public class PlayerPanel : Control
 			PlayerName.Text = string.Empty;
 			fishTexture.Texture = null;
 			Description.Text = "";
+			animation.Stop();
 		}
 		else
 		{
 			PlayerName.Text = player.AssignedDevice+"\n"+player.Fish.Name;
 			fishTexture.Texture = ResourceLoader.Load(player.Fish.SpriteFullPath()) as Texture;
 			Description.Text = player.Fish.ActionDescription;
+            fishTexture.RectPivotOffset = this.RectSize / 2.0f;
+
+            animation.Play("idle");
 		}
 	}
 }
